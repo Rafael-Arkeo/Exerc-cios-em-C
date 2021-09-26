bdfc7c9188189371f6dcdbccbbd7fcd9ba1ad483
rafa <arkerafa@gmail.com>
2021-09-26 17:50:05 -0300

    adicionando o programa raiz quadrada

---
+++ raiz_quadrada.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+    float a = 25;
+    float raiz = sqrt(a);
+
+    printf("%0.2f\n",raiz);
+
+
+    return 0;
+    
+    
+    
+    
+    
+
+
+}
